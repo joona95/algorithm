6b618a714595743ab43886204032ba6448be8420
joona95 <joona95@naver.com>
2021-02-23 00:12:32 +0900

    210222 31주차 콘테스트

--- 알고파/알고파/2178 미로 탐색.cpp
+++ 알고파/알고파/2178 미로 탐색.cpp
@@ -11,20 +11,21 @@ queue<pair<pair<int, int>, int>> q;
 
 int main() {
 	int n, m;
-	scanf("%d %d", &n, &m);
+	scanf("%d %d\n", &n, &m);
 
 	for (int i = 1; i <= n; i++) {
+		char c;
 		for (int j = 1; j <= m; j++) {
-			char c;
 			scanf("%c", &c);
 
 			if (c == '0') {
-				arr[i][j] = 0;
+				arr[i][j] = false;
 			}
 			else {
-				arr[i][j] = 1;
+				arr[i][j] = true;
 			}
 		}
+		scanf("%c", &c);
 	}
 
 	q.push(make_pair(make_pair(1, 1), 1));
@@ -33,22 +34,27 @@ int main() {
 	while (!q.empty()) {
 		pair<int, int> p = q.front().first;
 		cnt = q.front().second;
+		check[p.first][p.second] = true;
 		q.pop();
-
+		
 		if (p.first == n && p.second == m)
 			break;
 
-		if (p.first - 1 > 0 && arr[p.first - 1][p.second] && !check[p.first - 1][p.second]) {
-			q.push(make_pair(make_pair(p.first - 1, p.second), cnt + 1));
-		}
-		if (p.second - 1 > 0 && arr[p.first][p.second-1] && !check[p.first][p.second-1]) {
-			q.push(make_pair(make_pair(p.first, p.second-1), cnt + 1));
-		}
 		if (p.first + 1 <=n && arr[p.first + 1][p.second] && !check[p.first + 1][p.second]) {
 			q.push(make_pair(make_pair(p.first + 1, p.second), cnt + 1));
+			check[p.first + 1][p.second] = true;
 		}
 		if (p.second + 1 <= m && arr[p.first][p.second + 1] && !check[p.first][p.second + 1]) {
 			q.push(make_pair(make_pair(p.first, p.second + 1), cnt + 1));
+			check[p.first][p.second + 1] = true;
+		}
+		if (p.first - 1 > 0 && arr[p.first - 1][p.second] && !check[p.first - 1][p.second]) {
+			q.push(make_pair(make_pair(p.first - 1, p.second), cnt + 1));
+			check[p.first - 1][p.second] = true;
+		}
+		if (p.second - 1 > 0 && arr[p.first][p.second - 1] && !check[p.first][p.second - 1]) {
+			q.push(make_pair(make_pair(p.first, p.second - 1), cnt + 1));
+			check[p.first][p.second] = true;
 		}
 	}
 
