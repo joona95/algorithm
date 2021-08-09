dceb16e97fc02a7ff87dd4490e69da44af6a1b22
joona95 <joona95@naver.com>
2021-08-09 09:14:51 +0900

    BOJ10870_브론즈2 빌드 제외

--- baekjoon2/baekjoon2/10870 피보나치 수 5.cpp
+++ baekjoon2/baekjoon2/10870 피보나치 수 5.cpp
@@ -4,22 +4,6 @@
 
 using namespace std;
 
-/*
-int fibonacci[21];
-
-int main() {
-	int n;
-	scanf("%d", &n);
-	fibonacci[0] = 0;
-	fibonacci[1] = 1;
-	for (int i = 2; i <= n; i++) {
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-	}
-	printf("%d", fibonacci[n]);
-	return 0;
-}
-*/
-
 //Àç±Í
 int fibonacci(int n) {
 	if (n == 0)
@@ -35,3 +19,21 @@ int main() {
 	printf("%d", fibonacci(n));
 	return 0;
 }
+
+/*
+//dp
+int fibonacci[21];
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	fibonacci[0] = 0;
+	fibonacci[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+	}
+	printf("%d", fibonacci[n]);
+	return 0;
+}
+*/
+
