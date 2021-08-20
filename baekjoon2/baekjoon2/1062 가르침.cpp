919fe16f58eebf260ab3546a72518ef1d9260a30
joona95 <joona95@naver.com>
2021-08-20 11:20:47 +0900

    BOJ1062_골드4_완전탐색

--- baekjoon2/baekjoon2/1062 가르침.cpp
+++ baekjoon2/baekjoon2/1062 가르침.cpp
@@ -3,52 +3,68 @@
 #include <cstdio>
 #include <cstring>
 
+using namespace std;
+
 char word[55][20];
 bool visited[26];
 int n, k;
-int cnt = 0;
+int answer = 0;
 
-int DFS(int r) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 4; j < strlen(word[i]) - 4; j++) {
-
+void dfs(int r, int idx) {
+	if (idx == k) {
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			int l = 0;
+			int len = strlen(word[i]);
+			for (int k = 4; k < len-4; k++) { //앞 뒤로 anta / tica 제거
+				if (!visited[word[i][k] - 'a']) {
+					break;
+				}
+				l++;
+			}
+			if (l == len-8) {
+				cnt++;
+			}
+		}
+		if (answer < cnt) {
+			answer = cnt;
+		}
+		return;
+	}
+	/*
+	이부분을 main에서 한 번 시도해줄 때 0부터 시작해서 더 오래 걸린듯 
+	그래서 아예 main에서 진행이 안되도록 하거나 main도 dfs(0,idx+1)이 아니라 dfs(i+1, idx+1)로 해야했음
+	*/
+	for (int i = r; i < 26; i++) { //ABCD, BACD, BCAD 등 같은 동일한데 순서만 다른 여러 조합 방지
+		if (!visited[i]) {
+			visited[i] = true;
+			dfs(i + 1, idx + 1);
+			visited[i] = false;
 		}
 	}
 }
 
-
 int main() {
 	scanf("%d %d\n", &n, &k);
-
-	//a n t i c
+	for (int i = 0; i < n; i++) {
+		scanf("%s", word[i]);
+	}
 	visited['a' - 'a'] = true;
 	visited['c' - 'a'] = true;
 	visited['i' - 'a'] = true;
 	visited['n' - 'a'] = true;
 	visited['t' - 'a'] = true;
-
-	for (int i = 0; i < n; i++) {
-		scanf("%s", word[i]);
+	if (k < 5) {
+		printf("0");
+		return 0;
 	}
-
-	
-
-	for (int i = 0; i < 26; i++) {
-		if (visited[i] != true) {
-			DFS(i);
-		}
-	}
-
+	dfs(0, 5);
+	printf("%d", answer);
 	return 0;
 }
-//DFS?? 브루트포스
+//DFS 브루트포스
 
 
 /*
-(a)b(c)defgh(i)jklm(n)opqrs(t)uvwxyz
-anta rc tica=>r
-anta hello tica=>h e l o
-anta car tica=>r
-
-
+시간초과 때문에 너무 고생했다
 */
