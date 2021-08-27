eda622f6329611a31a49619df5ed990048519e75
joona95 <joona95@naver.com>
2021-08-28 00:08:11 +0900

    BOJ5046_실버5_구현

---
+++ BOJ/BOJ/5046 전국 대학생 프로그래밍 대회 동아리 연합.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+
+using namespace std;
+
+int n, b, h, w;
+int p;
+int min;
+
+int main() {
+	scanf("%d %d %d %d", &n, &b, &h, &w);
+	min = b+1;
+	for (int i = 0; i < h; i++) {
+		scanf("%d", &p);
+		for (int j = 0; j < w; j++) {
+			int a;
+			scanf("%d", &a);
+			if (n <= a && min > n*p) {
+				min = n * p;
+			}
+		}
+	}
+	if (min == b+1) {
+		printf("stay home");
+		return 0;
+	}
+	printf("%d", min);
+	return 0;
+}
+
+/*
+scanf 받는 도중에 continue, break 등을 넣어버렸더니 문제가 생겼었다!
+*/
