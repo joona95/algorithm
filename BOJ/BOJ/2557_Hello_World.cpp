899cbee1dd35a635d2baf4f4b5d9f28749ca6078
joona95 <joona95@naver.com>
2021-12-08 22:18:29 +0900

    BOJ2557_브론즈5_입출력

---
+++ BOJ/BOJ/2557_Hello_World.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello World!\n";
+    printf("Hello World!\n");
+    return 0;
+}
+
+/*
+cout 메모리 2020KB
+printf 메모리 1112KB
+확실히 cout이 메모리 더 잡아먹긴 함.
+*/
